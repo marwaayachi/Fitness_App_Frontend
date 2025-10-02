8b3028c328f77b12cef5f7bb02ca0099b4bdfd45
ayachimarwa <marwaayachi.25@gmail.com>
2025-10-02 20:01:53 +0100

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 
+      bg-[url('/backg.jpg')] bg-cover bg-center">
+      <main className="flex flex-col gap-[32px] row-start-2 text-white items-center sm:items-start">
         <h1 className="text-3xl font-bold mb-4">Welcome to Cheebo Fitness</h1>
         <p className="mb-6">Track workouts, set goals, and follow plans.</p>
         <div className="space-x-3">
